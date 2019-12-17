f2037c0d03aa020867dc3d133cbed278390d3b1f
Antonin <9219052+antonincms@users.noreply.github.com>
2019-12-17 20:26:50 +0100

    Renamed files into more meaningful names, and prettified code w/ black

---
+++ main.py
@@ -0,0 +1,39 @@
+import docker
+import time
+from server_mgmt import *
+
+if __name__ == "__main__":
+    tls_config = docker.tls.TLSConfig(
+        ca_cert="/root/.docker/ca.pem",
+        client_cert=("/root/.docker/cert.pem", "/root/.docker/key.pem"),
+    )
+    nb_csgo = 4
+    macvlan = False
+    image = "csgoserver"
+    client = docker.APIClient("tcp://172.16.1.3:2375", tls=tls_config)
+
+    print("Making networks")
+    if macvlan:
+        pool = docker.types.IPAMPool(subnet="192.168.0.0/24", gateway="192.168.0.1")
+        servers = [{"ip": "192.168.0.103", "interface": "bond0", "pool": pool}]
+        net = "macvlan-csgo"
+        db_ip = "192.168.0.176"
+        ebot_ip = "192.168.0.177"
+        ebotweb_ip = "192.168.0.178"
+        create_macvlan_network(servers, tls_config, net)
+    else:
+        servers = [{"ip": "172.16.1.3"}, {"ip": "172.16.1.4"}, {"ip": "172.16.1.5"}]
+        net = "host"
+        db_ip = "172.16.1.3"
+        ebot_ip = "172.16.1.3"
+        ebotweb_ip = "172.16.1.3"
+
+    print("Starting ebot containers")
+    with open("topology.csv", "w") as topo:
+        launch(client, nb_csgo, servers, db_ip, ebot_ip, ebotweb_ip, net, topo)
+        print("Deploying csgo servers")
+        deploy(tls_config, nb_csgo, servers, net, ebotweb_ip, image, topo)
+    print("Waiting for db to start...")
+    time.sleep(20)
+    print("Inserting servers into ebot db")
+    ebot_add_servers(servers, db_ip, net)
