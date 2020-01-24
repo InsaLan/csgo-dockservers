65bbfaa434b5c745348b98e55954461eab4270de
Antonin Camus <antonin.cms@gmail.com>
2020-01-24 21:59:28 +0100

    Fixed bugs, improved a bit code, and documented !
    Fixed bugs related to bad variables used or tokens still not fetched from yaml. Updated yaml template.
    Main.py should not open anymore any client to deploy ebot, to be consistent with other functions.
    Added documentation in readme related to docker TLS configuration (still to be improved).

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import docker
 import time
 import yaml
-from server_mgmt import *
+
+from server_mgmt import deploy_ebotserver, deploy_csgoserver, register_server_ebot
 
 if __name__ == "__main__":
+    print("Reading configuration")
     with open("config.yml", "r") as config_file:
-        config = yaml.load(config_file,Loader=yaml.FullLoader)
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
 
     tls_config = docker.tls.TLSConfig(
-        config["docker_tls"]
+        ca_cert=config["docker_tls"]["ca_cert"],
+        client_cert=config["docker_tls"]["client_cert"]
     )
     nb_csgo = config["csgo"]["nb_instances"]
     image = config["csgo"]["image_name"]
-    client = docker.APIClient("{}:{}".format(config["docker"]["connection"], config["docker"]["port"]), tls=tls_config)
-
-    print("Making networks")
     servers = config["host"]["csgo_servers_ip"]
     ebot_ip = config["host"]["ebot_ip"]
 
-    print("Starting ebot containers")
     with open("topology.csv", "w") as topo:
-        deploy_ebotserver(client, ebot_ip, topo)
+        print("Starting ebot containers")
+        deploy_ebotserver(ebot_ip, tls_config, topo)
         print("Deploying csgo servers")
-        deploy_csgoserver(nb_csgo, servers, ebot_ip, image, topo)
+        deploy_csgoserver(nb_csgo, servers, ebot_ip, image, tls_config, topo)
+
     print("Waiting for db to start...")
-    time.sleep(20)
+    time.sleep(20) #FIXME find a way to ping the DB instead of waiting a arbitrary amount of time
     print("Inserting servers into ebot db")
-    register_server_ebot(servers, ebot_ip)
+    register_server_ebot(servers, ebot_ip, tls_config)
